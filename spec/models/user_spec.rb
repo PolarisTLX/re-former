a13fade2ff00618448eb7dac654aab0607495aec
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-05-09 12:24:49 -0400

    applied rubocop fixes
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   fixtures :users
   subject { users(:one) }
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it "is not valid without a username" do
+  it 'is not valid without a username' do
     subject.username = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without an email" do
+  it 'is not valid without an email' do
     subject.email = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without an password" do
+  it 'is not valid without an password' do
     subject.password = nil
     expect(subject).to_not be_valid
   end
